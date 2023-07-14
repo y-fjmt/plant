10f3a84799cd5ea2a9f1d50e7b581cc4ec0d7b49
KanaFujitani <cattleya.leya0@gmail.com>
2023-07-14 18:09:17 +0900

    feat: create Sidebar

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,95 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Drawer from "@mui/material/Drawer";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import ViewListOutlinedIcon from "@mui/icons-material/ViewListOutlined";
+import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
+import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNoneOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
+import Toolbar from "@mui/material/Toolbar";
+
+const drawerWidth = 240;
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+export default function ResponsiveDrawer(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  const sidebar = (
+    <div>
+      <Toolbar />
+
+      <List>
+        {["Home", "View", "Edit", "Notify", "Logout"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index === 0 && <HomeOutlinedIcon />}
+                {index === 1 && <ViewListOutlinedIcon />}
+                {index === 2 && <EditOutlinedIcon />}
+                {index === 3 && <NotificationsNoneOutlinedIcon />}
+                {index === 4 && <LogoutOutlinedIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+
+        <Drawer
+          variant="permanent"
+          sx={{
+            display: { xs: "none", sm: "block" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+          open
+        >
+          {sidebar}
+        </Drawer>
+      </Box>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+        }}
+      ></Box>
+    </Box>
+  );
+}
