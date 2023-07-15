50f46e608a4d8f251c4536ba3b4f33d9fcc70ecf
KanaFujitani <cattleya.leya0@gmail.com>
2023-07-15 18:49:58 +0900

    feat: create viewpage

---
+++ src/pages/ViewPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ResponsiveDrawer from "@/components/Sidebar";
+import View from "@/components/View";
+
+const ViewPage = () => {
+  return (
+    <div>
+      <ResponsiveDrawer />
+      <View />
+    </div>
+  );
+};
+
+export default ViewPage;
