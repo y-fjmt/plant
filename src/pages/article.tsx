6a0d7aa6b56c28ec5bda79b884349f48577fbca0
Yudai Fujimoto <apricot4096@gmail.com>
2023-07-16 14:33:34 +0900

    feat: checkout feature

--- src/pages/article.tsx
+++ src/pages/article.tsx
@@ -22,9 +22,32 @@ type Data = {
 	ownUserId: string;
 };
 
+type Commit = {
+	content: string,
+	likes: 0,
+	articlePos: string,
+	userName: string,
+	commitId: string,
+	articleId: string,
+	ownUserId: string,
+	userId: string,
+	userIcon: string,
+	isPublic: boolean,
+	modified: string,
+	tags: string
+};
+
 type ContentList = {
 	pos: number
 	content: string
+	selecter: {
+		content: string,
+		likes: number,
+		userName: string,
+		userIcon: string,
+		modified: string,
+		tags: string
+	}[]
 };
 
 const MyComponent = () => {
@@ -36,6 +59,7 @@ const MyComponent = () => {
 
 	const [data1, setData1] = useState<Data>();
 	const [data2, setData2] = useState<ContentList[]>();
+	const [commits, setCommits] = useState<Commit[]>();
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -46,7 +70,14 @@ const MyComponent = () => {
 					}
 				});
 
+				const response2 = await axios.get('/commit', {
+					params: {
+						articleId: router.query['id']
+					}
+				});
+
 				setData1(response1.data);
+				setCommits(response2.data);
 
 			} catch (error) {
 				console.error(error);
@@ -69,7 +100,18 @@ const MyComponent = () => {
 			// # 
 			if(elem[0] == '#' && elem[1] == ' '){
 				if(current != ''){
-					contentList.push({pos: (Math.floor(pos * 10)) / 10, content: current})
+					contentList.push({
+						pos: (Math.floor(pos * 10)) / 10,
+						 content: current,
+						 selecter: [{
+							content: current,
+							likes: 0,
+							userName: data1.userName,
+							userIcon: data1.userIcon,
+							modified: data1.modified,
+							tags: '(main)'
+						}]
+						});
 					current = ''
 				}
 				current = elem;
@@ -80,7 +122,15 @@ const MyComponent = () => {
 			// ##
 			else if(elem[0] == '#' && elem[1] == '#' && elem[2] == ' '){
 				if(current != ''){
-					contentList.push({pos: (Math.floor(pos * 10)) / 10, content: current})
+					contentList.push({pos: (Math.floor(pos * 10)) / 10, content: current,
+					selecter: [{
+						content: current,
+						likes: 0,
+						userName: data1.userName,
+						userIcon: data1.userIcon,
+						modified: data1.modified,
+						tags: '(main)'
+					}]})
 					current = ''
 				}
 				pos += 0.1
@@ -92,9 +142,33 @@ const MyComponent = () => {
 				current += '\n\n'+elem
 			}
 		});
-		contentList.push({pos: (Math.floor(pos * 10)) / 10, content: current});
+		contentList.push({pos: (Math.floor(pos * 10)) / 10, content: current,
+		selecter: [{
+			content: current,
+			likes: 0,
+			userName: data1.userName,
+			userIcon: data1.userIcon,
+			modified: data1.modified,
+			tags: '(main)'
+		}]});
 
 		setData2(contentList);
+		console.log(commits);
+
+		commits?.forEach(commit => {
+			for (let i = 0; i < contentList.length; i++) {
+				if(commit.articlePos === contentList[i].pos.toString()){
+					contentList[i].selecter.push({
+						content: commit.content,
+						likes: commit.likes,
+						userName: commit.userName,
+						userIcon: commit.userIcon,
+						modified: commit.modified,
+						tags: commit.tags
+					});
+				}	
+			}
+		});
 		}
 	}, [data1]);
 
@@ -127,11 +201,33 @@ const MyComponent = () => {
 					</div>
 					<table>
 						{
-							data2.map(elem => {
+							data2.map((elem, i) => {
 								return (
 									<tr>
 										<td><Link href={`/commit?id=${router.query['id']}&pos=${elem.pos}&name=${data1.tags}&owner=${data1.ownUserId}`}>+</Link></td>
-										<td>{ elem.pos }</td>
+										<td>
+											<select onChange={event => {
+												const index:number = parseInt(event.target.value);
+												var tmp = data2.concat();
+												tmp[i].content = tmp[i].selecter[index].content;
+												setData2(tmp);
+											}}>
+												{
+													elem.selecter.map((op, index) => {
+														return(
+															<option value={index}>
+																<div className="flex">
+																	<img className="w-10 h-10" src={op.userIcon} alt="" /><div>{ op.userName }/{ op.tags }</div>
+																</div>
+																<div>
+																{ op.modified } / 
+																</div>
+															</option>
+														);
+													})
+												}
+											</select>
+										</td>
 										<td><div className="markdown-body m-5 ">
 						<ReactMarkdown className="p-4">{elem.content}</ReactMarkdown>
 					</div></td>
