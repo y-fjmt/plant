88be153276cfb165fc5d6a69e3973a873a5d4492
KanaFujitani <cattleya.leya0@gmail.com>
2023-07-16 10:08:03 +0900

    feat: disable likes button

--- src/components/Listitem.tsx
+++ src/components/Listitem.tsx
@@ -1,18 +1,17 @@
 import * as React from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 type Props = {
-  img: string
-  title: string
-  auther: string
-  articleId: string
-  likes: number
+  img: string;
+  title: string;
+  auther: string;
+  articleId: string;
+  likes: number;
 };
 
 const Listitem: React.FC<Props> = (props) => {
-
   const router = useRouter();
-  
+
   return (
     <div>
       <ul className="max-w-md divide-y divide-gray-200 mx-10 mt-10">
@@ -29,17 +28,22 @@ const Listitem: React.FC<Props> = (props) => {
               <p className="text-sm font-medium text-blue-900 truncate ">
                 {props.title}
               </p>
-              <p className="text-sm text-gray-500 truncate">
-                {props.auther}
-              </p>
+              <p className="text-sm text-gray-500 truncate">{props.auther}</p>
             </div>
           </div>
           <div className="space-x-2">
-            <button className="bg-emerald-600 hover:bg-emerald-700 text-white font-semibold py-2 px-4 rounded-full">
+            {/* <button className="bg-emerald-600 hover:bg-emerald-700 text-white font-semibold py-2 px-4 rounded-full">
               &nbsp; likes{props.likes} &nbsp;
-            </button>
-            <button onClick={() => {router.push(`/article?id=${props.articleId}`);}}
-            className="bg-emerald-600 hover:bg-emerald-700 text-white font-semibold py-2 px-4 rounded-full">
+            </button> */}
+            <label className="bg-emerald-600 text-white font-semibold py-2.5 px-4 rounded-full">
+              &nbsp; いいね 0 &nbsp;
+            </label>
+            <button
+              onClick={() => {
+                router.push(`/article?id=${props.articleId}`);
+              }}
+              className="bg-emerald-600 hover:bg-emerald-700 text-white font-semibold py-2 px-4 rounded-full"
+            >
               &nbsp; 読む &nbsp;
             </button>
           </div>
